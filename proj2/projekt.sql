1ba9a8ba98c6acca214ec20642fb34b9d7f79a86
MrDalo <dadokralik@gmail.com>
2022-03-27 21:41:40 +0200

    SQL script without c_uctu validation

--- proj2/projekt.sql
+++ proj2/projekt.sql
@@ -1,30 +1,30 @@
 -- Zadanie: 26. Banka
 -- Autor: Patrik Sehnoutek (xsehno01), Dalibor Králik (xkrali20)
 
-DROP TABLE VYPIS CASCADE CONSTRAINTS;
 DROP TABLE PRIKAZ CASCADE CONSTRAINTS;
-DROP TABLE ZAMESTNANEC CASCADE CONSTRAINTS;
-DROP TABLE UCET CASCADE CONSTRAINTS;
+DROP TABLE VYPIS CASCADE CONSTRAINTS;
 DROP TABLE SPRAVUJE CASCADE CONSTRAINTS;
-DROP TABLE KLIENT CASCADE CONSTRAINTS;
+DROP TABLE ZAMESTNANEC CASCADE CONSTRAINTS;
 DROP TABLE DISPONUJE CASCADE CONSTRAINTS;
+DROP TABLE UCET CASCADE CONSTRAINTS;
+DROP TABLE KLIENT CASCADE CONSTRAINTS;
 
 -- TODO: osetrenie rodneho cisla a zmena dlzky
 -- TODO: podrobnejsie osetrenie IBAN-u
 
 CREATE TABLE KLIENT(
-    ID_klient INTEGER NOT NULL,
-    meno VARCHAR(30),04
-    priezvisko VARCHAR(30),
+    ID_klient INTEGER GENERATED AS IDENTITY NOT NULL,
+    meno VARCHAR(40),
+    priezvisko VARCHAR(40),
     ulica VARCHAR(255),
     mesto VARCHAR(255),
-    email VARCHAR(50),
-    telefon VARCHAR(9), CHECK(REGEXP_LIKE(telefon, '^[0-9]{9}$')),
+    email VARCHAR(255),
+    telefon VARCHAR(9) CHECK(REGEXP_LIKE(telefon, '^[0-9]{9}$')),
     rodne_cislo VARCHAR(11),
-    datum_narodenia DATE,
+    datum_narodenia DATE NOT NULL,
 
-    CONSTRAINT PK_ID_klient PRIMARY KEY
-)
+    CONSTRAINT PK_ID_klient PRIMARY KEY (ID_klient)
+);
 
 CREATE TABLE UCET (
     c_uctu NUMBER NOT NULL,
@@ -35,8 +35,8 @@ CREATE TABLE UCET (
     poplatok DECIMAL(10,3),
     ID_klient INTEGER NOT NULL,
     CHECK (
-        ((urok == NULL) and (poplatok != NULL) or
-        (urok != NULL) and (poplatok == NULL))
+        ((urok is NULL) and (poplatok is not NULL) or
+        (urok is not NULL) and (poplatok is NULL))
         ),
 
     CONSTRAINT PK_c_uctu PRIMARY KEY (c_uctu),
@@ -51,21 +51,21 @@ CREATE TABLE DISPONUJE(
     CONSTRAINT FK_c_uctu_disponent FOREIGN KEY (c_uctu) REFERENCES UCET,
     CONSTRAINT FK_ID_disponent FOREIGN KEY (ID_klient) REFERENCES KLIENT,
     CONSTRAINT PK_disponuje PRIMARY KEY (c_uctu, ID_klient)
-)
+);
 
 CREATE TABLE ZAMESTNANEC(
-    ID_zamestanec INTEGER NOT NULL,
-    meno VARCHAR(30),
-    priezvisko VARCHAR(30),
+    ID_zamestanec INTEGER GENERATED AS IDENTITY NOT NULL,
+    meno VARCHAR(40),
+    priezvisko VARCHAR(40),
     ulica VARCHAR(255),
     mesto VARCHAR(255),
-    email VARCHAR(50),
+    email VARCHAR(255),
     telefon VARCHAR(9), CHECK(REGEXP_LIKE(telefon, '^[0-9]{9}$')),
     rodne_cislo VARCHAR(11),
-    datum_narodenia DATE,
+    datum_narodenia DATE NOT NULL,
 
-    CONSTRAINT PK_ID_zamestnanec PRIMARY KEY
-)
+    CONSTRAINT PK_ID_zamestnanec PRIMARY KEY (ID_zamestanec)
+);
 
 CREATE TABLE SPRAVUJE (
     ID_zamestnanec INTEGER NOT NULL,
@@ -74,43 +74,43 @@ CREATE TABLE SPRAVUJE (
     CONSTRAINT FK_ID_zamestnanec FOREIGN KEY (ID_zamestnanec) REFERENCES ZAMESTNANEC,
     CONSTRAINT FK_c_uctu FOREIGN KEY (c_uctu) REFERENCES UCET,
     CONSTRAINT PK_spravuje PRIMARY KEY (ID_zamestnanec, c_uctu)
-)
+);
 
 CREATE TABLE VYPIS (
-    poradove_cislo INTEGER NOT NULL,
+    poradove_cislo INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
     datum_zalozenia DATE NOT NULL,
     c_uctu NUMBER NOT NULL,
 
     CONSTRAINT PK_poradove_cislo_vypis PRIMARY KEY (poradove_cislo, c_uctu),
-    CONSTRAINT FK_c_uctu_vypis FOREIGN KEY (cislo) REFERENCES UCET
+    CONSTRAINT FK_c_uctu_vypis FOREIGN KEY (c_uctu) REFERENCES UCET
 );
 
 CREATE TABLE PRIKAZ(
-    poradove_cislo INTEGER NOT NULL,
+    poradove_cislo INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL,
     datum DATE NOT NULL,
     ciastka DECIMAL(20, 2),
     typ VARCHAR(50),
     c_uctu NUMBER NOT NULL,
 
     CONSTRAINT PK_poradove_cislo_prikaz PRIMARY KEY (poradove_cislo, c_uctu),
-    CONSTRAINT FK_c_uctu_prikaz FOREIGN KEY (cislo) REFERENCES UCET
+    CONSTRAINT FK_c_uctu_prikaz FOREIGN KEY (c_uctu) REFERENCES UCET
 );
 
 -- Proj2 - vlozenie vzorovych dat
 
 -- TODO: zmenit hodnoty, ak budeme pridavat nejake osetrenia
-INSERT INTO KLIENT(ID_klient, meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
-VALUES('123456789', 'Patrik', 'Sehnoutek', 'Purkynova', 'Brno', 'patrik@gmail.com', '774589123', '040503/0010', TO_DATE('2004-05-03', 'YYYY-MM-DD'));
-INSERT INTO KLIENT(ID_klient, meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
-VALUES('987654321', 'Dalibor', 'Kralik', 'Purkynova', 'Brno', 'dalibor@gmail.com', '4562563124', '030201/0016', TO_DATE('2003-02-01', 'YYYY-MM-DD'));
-INSERT INTO KLIENT(ID_klient, meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
-VALUES('11122233', 'Jan', 'Novak', 'Staromestska', 'Praha', 'jan@centrum.cz', '445322564', '011210/3356', TO_DATE('2001-12-10', 'YYYY-MM-DD'));
-INSERT INTO KLIENT(ID_klient, meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
-VALUES('444555666', 'Anna', 'Novakova', 'Staromestska', 'Praha', 'anna@gmail.com', '987562365', '025707/3366', TO_DATE('2002-07-07', 'YYYY-MM-DD'));
+INSERT INTO KLIENT( meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
+VALUES('Patrik', 'Sehnoutek', 'Purkynova', 'Brno', 'patrik@gmail.com', '774589123', '040503/0010', TO_DATE('2004-05-03', 'YYYY-MM-DD'));
+INSERT INTO KLIENT( meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
+VALUES('Dalibor', 'Kralik', 'Purkynova', 'Brno', 'dalibor@gmail.com', '456253124', '030201/0016', TO_DATE('2003-02-01', 'YYYY-MM-DD'));
+INSERT INTO KLIENT( meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
+VALUES('Jan', 'Novak', 'Staromestska', 'Praha', 'jan@centrum.cz', '445322564', '011210/3356', TO_DATE('2001-12-10', 'YYYY-MM-DD'));
+INSERT INTO KLIENT( meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
+VALUES('Anna', 'Novakova', 'Staromestska', 'Praha', 'anna@gmail.com', '987562365', '025707/3366', TO_DATE('2002-07-07', 'YYYY-MM-DD'));
 
-INSERT INTO ZAMESTNANEC(ID_zamestanec, meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
-VALUES('123456', 'Vladimir', 'Stary', 'Nadrazni', 'Brno', 'vladimir@banka.cz', '458695245', '846014/1008', TO_DATE('1984-10-14', 'YYYY-MM-DD'));
-INSERT INTO ZAMESTNANEC(ID_zamestanec, meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
-VALUES('654321', 'Alena', 'Nova', 'Purkynova', 'Brno', 'alena@banka.cz', '362154783', '936105/2569', TO_DATE('1993-11-05', 'YYYY-MM-DD'));
+INSERT INTO ZAMESTNANEC( meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
+VALUES('Vladimir', 'Stary', 'Nadrazni', 'Brno', 'vladimir@banka.cz', '458695245', '846014/1008', TO_DATE('1984-10-14', 'YYYY-MM-DD'));
+INSERT INTO ZAMESTNANEC( meno, priezvisko, ulica, mesto, email, telefon, rodne_cislo, datum_narodenia)
+VALUES('Alena', 'Nova', 'Purkynova', 'Brno', 'alena@banka.cz', '362154783', '936105/2569', TO_DATE('1993-11-05', 'YYYY-MM-DD'));
 
 
